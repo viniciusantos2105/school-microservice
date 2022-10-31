5e232003e6a202f9e6477bbeeb92756407510401
Vinicius Jesus <92672912+viniciusantos2105@users.noreply.github.com>
2022-10-31 11:25:54 -0300

    add subscribe api

---
+++ subscribe-api/src/main/java/com/schoolmicroservice/subscribeapi/SubscribeApiApplication.java
@@ -0,0 +1,13 @@
+package com.schoolmicroservice.subscribeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscribeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscribeApiApplication.class, args);
+	}
+
+}
---
+++ subscribe-api/src/test/java/com/schoolmicroservice/subscribeapi/SubscribeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schoolmicroservice.subscribeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscribeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
